be080061f874c858d47f8fabebfa28b9c5301713
Luka Avbreht <luka.avbreht@student.fmf.uni-lj.si>
2018-10-24 15:15:03 +0200

    vaja 5

--- Naloga5/beziersub.m
+++ Naloga5/beziersub.m
@@ -1,23 +1,36 @@
-function  BS = beziersub(B,t)
-% Opis: beziersub  izvede  subdivizijo  Bezierjeve  krivulje
+function BS = beziersub (B , t, k)
+% Opis : beziersub izvede subdivizijo Bezierjeve krivulje
 %
-% Definicija:
-%   BS = beziersub(B,t)
+% Definicija :
+% BS = beziersub (B,t)
 %
-% Vhodni  podatki:
-%   B
-% matrika  kontrolnih  tock  Bezierjeve  krivulje , v
-%
-% kateri  vsaka  vrstica  predstavlja  eno  kontrolno
-%
+% Vhodni podatki :
+% B matrika kontrolnih tock Bezierjeve krivulje , v
+% kateri vsaka vrstica predstavlja eno kontrolno
 % tocko ,
-%   t
-% parameter  subdivizije  Bezierjeve  krivulje
+% t parameter subdivizije Bezierjeve krivulje
 %
-% Izhodni  podatek:
-%   BS    celica , ki  vsebuje  kontrolne  tocke  dveh  krivulj , ki
-%
-% jih  dobimo s subdivizijo  prvotne  Bezierjeve  krivulje
+% Izhodni podatek :
+% BS celica , ki vsebuje kontrolne tocke dveh krivulj , ki
+% jih dobimo s subdivizijo prvotne Bezierjeve krivulje
 
+n = length(B);
+BS = cell(1, 2^k);
+
+d1 = decasteljau(B(:, 1), t);
+d2 = decasteljau(B(:, 2), t);
+
+BS{1} = [d1(1,:)' d2(1,:)'];
+BS{2^(k-1)+1} = [diag(fliplr(d1)) diag(fliplr(d2))];
+
+for i=2:k
+    for j=1:2^(k-i+1):2^k
+        d1 = decasteljau(BS{j}(:, 1), t);
+        d2 = decasteljau(BS{j}(:, 2), t);  
+        
+        BS{j} = [d1(1,:)' d2(1,:)'];
+        BS{j+2^(k-i)} = [diag(fliplr(d1)) diag(fliplr(d2))];
+    end
+end
 
 end
